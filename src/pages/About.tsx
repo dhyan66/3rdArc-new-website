aaa6a55463bc62bd585456bf23b92c32c104901e
dhyan66 <94802145+dhyan66@users.noreply.github.com>
2026-02-06 18:07:58 -0400

    about section fix

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -84,15 +84,6 @@ const About = () => {
                 CREATIVE PRODUCTION & PHOTOGRAPHY
               </p>
             </div>
-
-            {/* Portrait */}
-            <div className="max-w-md mx-auto border border-foreground/10 overflow-hidden">
-              <img
-                src="/Owners.png"
-                alt="3rd Arc Productions Team"
-                className="w-full h-auto"
-              />
-            </div>
           </div>
 
           {/* Bio Section */}
